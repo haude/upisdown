d2cc3ab90f67a84bf6681a5463ee0e6463431688
rho <rho.rhoit@gmail.com>
2013-05-20 21:28:20 +0545

    upper-case

--- README.md
+++ README.md
@@ -3,20 +3,22 @@
 transform text into mirror image!
 
 * vertical
-* horizontal
 
-more info at [wikipedia][1]
+more info at
+* [wikipedia][1]
+* [unicode table][2]
 
 #### HOW-TO USE
 
 	$ ./main.sh "<text! to be entered>"
 	<pǝɹǝʇuǝ ǝq oʇ ¡ʇxǝʇ>
-	
-	
+
+
 #### Quick Test
-     
+
 	 $ make
 	 ./main.sh "test <string>? banquet & pink!"
 	 ¡ʞuıd ⅋ ʇǝnbuɐq ¿<ƃuıɹʇs> ʇsǝʇ
-	
+
 [1]: http://en.wikipedia.org/wiki/Transformation_of_text
+[2]: "http://unicode-table.com/en/"
--- main.sh
+++ main.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# TODO: Upper case letters
-# TODO: symbols and punctuation
 # TODO: read from pipe
 
 PRG=$(basename $0)
@@ -13,40 +11,57 @@ function Usage {
 
 function map {
     case $flip in
-	'a') echo -n "ɐ"; continue;;
-	'b') echo -n "q"; continue;;
-	'c') echo -n "ɔ"; continue;;
-	'd') echo -n "p"; continue;;
-	'e') echo -n "ǝ"; continue;;
-	'f') echo -n "ɟ"; continue;;
-	'g') echo -n "ƃ"; continue;;
-	'h') echo -n "ɥ"; continue;;
-	'i') echo -n "ı"; continue;;
-	'j') echo -n "ɾ"; continue;;
-	'k') echo -n "ʞ"; continue;;
-	'l') echo -n "ן"; continue;;
-	'm') echo -n "ɯ"; continue;;
-	'n') echo -n "u"; continue;;
-	#'o') echo -n "o"; continue;;
-	'p') echo -n "d"; continue;;
-	'q') echo -n "b"; continue;;
-	'r') echo -n "ɹ"; continue;;
-	#'s') echo -n "s"; continue;;
-	't') echo -n "ʇ"; continue;;
-	'u') echo -n "n"; continue;;
-	'v') echo -n "ʌ"; continue;;
-	'w') echo -n "ʍ"; continue;;
-	#'x') echo -n "x"; continue;;
-	'y') echo -n "ʎ"; continue;;
-	#'z') echo -n "z"; continue;;
+	# visit "http://unicode-table.com/en/" for more info3
+	'a') echo -n "ɐ"; continue;; 'A') echo -n "∀"; continue;;
+	'b') echo -n "q"; continue;; 'B') echo -n "q"; continue;; #
+	'c') echo -n "ɔ"; continue;; 'C') echo -n "Ɔ"; continue;;
+	'd') echo -n "p"; continue;; 'D') echo -n "p"; continue;; #
+	'e') echo -n "ǝ"; continue;; 'E') echo -n "Ǝ"; continue;; #Ǝ∃
+	'f') echo -n "ɟ"; continue;; 'F') echo -n "߃"; continue;;
+	'g') echo -n "ƃ"; continue;; 'G') echo -n "⅁"; continue;;
+	'h') echo -n "ɥ"; continue;;#'H') echo -n "H"; continue;;
+	'i') echo -n "ı"; continue;;#'I') echo -n "I"; continue;;
+	'j') echo -n "ɾ"; continue;; 'J') echo -n "ɾ"; continue;; #
+	'k') echo -n "ʞ"; continue;; 'K') echo -n "ʞ"; continue;; #
+	'l') echo -n "⅂"; continue;; 'L') echo -n "˥"; continue;; # Γ
+	'm') echo -n "ɯ"; continue;; 'M') echo -n "W"; continue;;
+	'n') echo -n "u"; continue;; 'N') echo -n "Ͷ"; continue;; # И
+#	'o') echo -n "o"; continue;; 'O') echo -n "O"; continue;;
+	'p') echo -n "d"; continue;; 'P') echo -n "d"; continue;; #
+	'q') echo -n "b"; continue;; 'Q') echo -n "b"; continue;; #
+	'r') echo -n "ɹ"; continue;; 'R') echo -n "ᴚ"; continue;; #ᴚ
+#	's') echo -n "s"; continue;; 'S') echo -n "S"; continue;;
+	't') echo -n "ʇ"; continue;; 'T') echo -n "⊥"; continue;;
+	'u') echo -n "n"; continue;; 'U') echo -n "⋂"; continue;;
+	'v') echo -n "ʌ"; continue;; 'V') echo -n "Λ"; continue;; #∧ʌ⋀Λ
+	'w') echo -n "ʍ"; continue;; 'W') echo -n "M"; continue;;
+#	'x') echo -n "x"; continue;; 'X') echo -n "X"; continue;;
+	'y') echo -n "ʎ"; continue;; 'Y') echo -n "⅄"; continue;;
+#	'z') echo -n "z"; continue;; 'Z') echo -n "Z"; continue;;
 
 	'&')  echo -n "⅋"; continue;;
 	'?') echo -n "¿"; continue;;
 	'!') echo -n "¡"; continue;;
-	'<') echo -n ">"; continue;;
-	'>') echo -n "<"; continue;;
+	'<') echo -n ">"; continue;; '>') echo -n "<"; continue;;
+	'{') echo -n "}"; continue;; '}') echo -n "{"; continue;;
+	'/') echo -n '\'; continue;; '\') echo -n '/'; continue;;
+
+	'^') echo -n "ˬ"; continue;;
+	',') echo -n "ˋ"; continue;;#ˊˋ̒
+
+#	'0') echo -n "v"; continue;;
+	'1') echo -n "⥝"; continue;;
+	'2') echo -n "ⵒ"; continue;;
+	'3') echo -n "ɛ"; continue;;
+	#'4') echo -n "4"; continue;;
+	#'5') echo -n "5"; continue;;
+	'6') echo -n "9"; continue;;
+	'7') echo -n "⦣"; continue;;
+#	'8') echo -n "8"; continue;;
+	'9') echo -n "6"; continue;;
+
 	*) echo -n $flip;;
-    esac	
+    esac
 }
 
 # checking arguments
@@ -59,7 +74,7 @@ text="$@" # $@ can't be used for counting
 
 IFS="" #don't ignore spaces
 
-for ((i=${#text}; i >= 0; i--)); do    
+for ((i=${#text}; i >= 0; i--)); do
     flip=${text:$i:1}
     map
 done
